e94a5139a0486cf9e044e42840f8a3e5e532da96
Ayesha Anwar <official.ayeshaanwar107@gmail.com>
2025-04-30 17:08:47 -0700

    datatypes

---
+++ practice day 1/datatype.js
@@ -0,0 +1,15 @@
+/* datatypes
+   
+   number =>2 to power 53
+   string = ""
+   boolean = true/false
+   null => stand alone value
+   undefined have its output as undefined because it is its type
+   symbol = unique
+
+*/
+
+// object
+
+console.log(typeof undefined); //output = undefined
+console.log(typeof null);
