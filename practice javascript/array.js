80040a567f3d52662fd509e56ad05255e8c6f345
Ayesha Anwar <official.ayeshaanwar107@gmail.com>
2025-05-01 15:25:13 -0700

    built in function

--- practice javascript/array.js
+++ practice javascript/array.js
@@ -65,5 +65,99 @@ mrr.map((number,index) =>{
 }
 )
 
-let yourname= 'ayesha'
-console.log(yourname)
+// filter is used to seprate the required numbeer
+
+let num2=[2,3,4,8,9,11,16]
+let evennum = num2.filter((number)=>{
+    // we can use as condition also as
+    // if (number%2==0){
+    //     return true;
+    // }
+    // else{
+    //     return false
+    // }
+    
+    // we can write it other way without condition
+    return number%2==0;
+    // we can also use this filter acording to type
+});
+console.log(evennum);
+
+
+// reduce use acccumulator and current variable to show signle answer of array
+let nrr= [10, 20,,30,40]
+let myanswer = nrr.reduce((acc, curr)=>{
+     return acc + curr;
+}, 0);
+console.log(myanswer)
+
+
+// sorting 
+
+let set=[3,8,5,1,4]
+set.sort();
+console.log(set);
+
+// sorting in decending order
+
+set.sort()
+set.reverse()
+console.log(set)
+
+// index is used to print the index of some value
+
+let set2=[3,6,9,12,16,19,20]
+let correctans = set2.indexOf(19);
+console.log(set2)
+console.log(correctans);
+
+
+// for each is used to show the value,index etc of each number
+// also find length
+
+let score=[10,20,30,40,50]
+let length = score.length;
+console.log("length: " ,length);
+
+
+score.forEach((num,index)=>{
+    console.log("number: " ,num , "index: " , index);
+})
+
+// print array by using traditional loop
+
+let tra=[2,3,4,5,6,7]
+let lengthe = tra.length;
+console.log("length: ", lengthe);
+
+
+for(let index=0; index<=length; index++){
+    console.log(tra[index])
+}
+
+// for in is used to print series like object with their values as well
+
+let object ={
+
+    name: "ayesha",
+    table: 4,
+    cgp: 3.25,
+
+}
+
+for(let series in object) {
+    console.log(series, " ", object[series]);
+}
+
+
+// for of is used to itrate the array
+let zrr=[1,2,3,4];
+for (let value of zrr){
+    console.log(value)
+}
+
+let fulname = "Ayesha"
+
+for(let val of fulname){
+    console.log(val);
+}
