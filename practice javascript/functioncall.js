7a76186d5f8aa3c335c945591e6841730a3c3955
Ayesha Anwar <official.ayeshaanwar107@gmail.com>
2025-05-03 13:53:20 -0700

    pass functin

--- practice javascript/functioncall.js
+++ practice javascript/functioncall.js
@@ -5,10 +5,44 @@
 console.log(age);
 var age = 19;
 
-// in the state of function when we call before declaration and defination it will return the acurate answer
+// (function hoisting)in the state of function when we call before declaration and defination it will return the acurate answer
 // it will shift the whole function to the top of calling
 sayname();
 
 function sayname(){
     console.log("ayesha")
 }
+
+
+// stack store the data in the form LIFO (last in first out)
+/*
+    why function is called the first class citizen because:
+        assign to variable
+         pass as an argument
+         return functin
+         use function in data structure (as an array)
+         as property
+*/
+
+
+// assign functin to variable(function expression)
+let greet = function(){
+    console.log("greeting and blessings");
+}
+greet()
+
+
+// how to passs function
+
+function hello(){
+    console.log("hello ayesha");
+    salam();
+}
+
+function salam(){
+    console.log("salam to all")
+}
+hello();
+
+
+// how to return functon
