f4cc341d59d1120b112bf7cb4eb06e1598151433
Ayesha Anwar <official.ayeshaanwar107@gmail.com>
2025-05-05 15:09:49 -0700

    inbuilt objects

---
+++ practice javascript/inbuilt object .js
@@ -0,0 +1,22 @@
+// it will give in built math operations like pi, max, min, roundof etc
+
+console.log(Math.PI);
+
+console.log(Math.pow(2,5));
+
+console.log(Math.sqrt(25));
+
+console.log(Math.round(1.9));
+
+console.log(Math.pow(5, 2));
+
+
+// genrate date and months and years
+let curr = new Date();
+console.log(curr);
+
+let date = new Date('july 10 2005')
+console.log(date);
+
+let newDate = new Date (2005 , 6, 10);
+console.log(newDate)
