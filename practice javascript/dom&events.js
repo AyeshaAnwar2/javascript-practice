9bb9d2d28209b944387dd4cb5d48e7f3feb419f4
Ayesha Anwar <official.ayeshaanwar107@gmail.com>
2025-05-10 15:35:24 -0700

    phases of events

--- practice javascript/dom&events.js
+++ practice javascript/dom&events.js
@@ -1,22 +1,45 @@
 
-//    we can use .style to set and get the single property of thh code
+// //    we can use .style to set and get the single property of thh code
 let firstclass = document.getElementById('1para')
 firstclass.style.backgroundColor = "pink";
 
 
-//    style.cssText is used to get or set t he multiple porperties at a time like background color,color,margin etc
+// //    style.cssText is used to get or set t he multiple porperties at a time like background color,color,margin etc
 let para2 = document.getElementById('2para')
 para2.style.cssText = "background-color:red; color:white; padding:0.2rem";
 
 
-//    .setAttribute is used to set the attributes like class name or set the margins etc
+// //    .setAttribute is used to set the attributes like class name or set the margins etc
 let firstd = document.getElementById('fdiv')
 firstd.setAttribute("color", "orange");
 
 
-//    .className is used to add or fetch multiple classes
+// //    .className is used to add or fetch multiple classes
 
-//    .classlist is used to add, remove, toggle or constain the whole classes
+// //    .classlist is used to add, remove, toggle or constain the whole classes
 function addclass(){
     para2.classList.add("highlight");
-}
+//     // same we can use remove or toggle aslo
+ }
+
+
+//                             EVENTS
+
+/* events (we can say that it is action which happen on our clicks , our click is called evnet , on wich
+we click is called event target and what happen after the click is called action or behaviour )
+ actions are written in the event listner
+
+ syntax:       <event-target>.addEventListner (<event type> , <function ,action>)
+*/
+
+
+
+function checkchange(){
+    let heading = document.getElementById('events');
+    heading.textContent = " hello Ayesha"
+ }
+
+let heading = document.getElementById('events');
+
+heading.addEventListener('click' , checkchange);
+// heading.removeEventListener('click' , checkchange)
