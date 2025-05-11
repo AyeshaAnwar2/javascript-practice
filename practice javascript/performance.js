ba3226f7e1e2a646e2977f3b656b0e491c301d41
Ayesha Anwar <official.ayeshaanwar107@gmail.com>
2025-05-11 07:42:36 -0700

    fragment method

--- practice javascript/performance.js
+++ practice javascript/performance.js
@@ -34,3 +34,41 @@ document.body.appendChild(checkdiv)
 
 const t4 = performance.now()
 console.log("total time = " +(t4-t3))
+
+
+/*
+
+we can see that our second code is runnig in less time it is because it has less reflow and repaint ti;me
+reflow is the calculation of the whole process
+repaint is jsut the displayin code result
+reflow happen in every single line when we append it in the document body
+so in the first code we append eveny single line to the document body wich take the more reflow and repaint so it take more time to relode
+while in the second code we just append the whole code in the div first line by line then append the whole div in the document body in a single take 
+which takes the less time
+
+in short
+
+less reflow and repaint = less time taken (fast loading)
+
+*/
+
+
+
+
+// best way
+// document fragment which dont take no reflow no repaint but take 1 reflow and one retake when add to document
+
+const t5 = performance.now()
+
+let fragment = document.createDocumentFragment();
+ 
+for (let i = 1; i<50; i++){
+    let para = document.createElement('p')
+    para.textContent = ("this is para" +i +"ab") 
+    // no fragment no repaint
+    fragment.appendChild(para);
+}
+// 1 reflow and 1 repaint
+document.body.appendChild(fragment);
+const t6 = performance.now()
+console.log("check time" +(t6-t5));
