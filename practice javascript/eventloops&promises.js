a3b8f48db5acc1775861c3159475e872019b56ac
Ayesha Anwar <official.ayeshaanwar107@gmail.com>
2025-05-13 16:57:18 -0700

    promise all

--- practice javascript/eventloops&promises.js
+++ practice javascript/eventloops&promises.js
@@ -20,30 +20,23 @@
 // syntax : let firstpromise = new promise((resolve, reject) =>{
 // ......function............})
 
-let firstpromise = new Promise((resolve , reject) =>{
 
-    setTimeout(function saymyName(){
-    console.log("my name is Ayesha Anwar");
-}, 15000);
-   resolve("fulfilled")
+
+// we can use promise.all to print multiple promis
+
+let promise1 = new Promise((resolve, reject)=>{
+    setTimeout(resolve, 1000 , "first");
 })
 
-// then and catch are used in promise if its true
-// use then when resolve , catch when rejected
-
-let mypromise = new Promise((resolve , reject)=>{
-  let success =true ;
-  if (success)(
-    resolve("promise fulfilled")
-  )
-  else{
-    reject("pormise rejected")
-  }
-});
-
-mypromise.then((message)=>{
-    console.log("message of then" +message);
+let promise2 = new Promise((resolve , reject)=>{
+    setTimeout(resolve , 2000 , "second");
 })
-mypromise.catch((error)=>{
-    console.log("Error:" +error);
+
+let promise3 = new Promise((resolve , reject)=>{
+    setTimeout(resolve , 3000 , "third");
+})
+
+Promise.all([promise1 , promise2 , promise3])
+.then((values)=>{
+    console.log(values)
 })
